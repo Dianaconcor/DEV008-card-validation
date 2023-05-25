2171cb12a6a3eb6b478c85b69a15491615ef0608
Diana contreras <dianacontreras@MacBook-Pro-de-Diana.local>
2023-05-25 12:58:58 -0600

    MEDIO TERMINADO

---
+++ README2.md
@@ -0,0 +1,22 @@
+# Tarjeta de credito valida 
+
+# INDICE 
+1. img
+2. index.html
+3. index.js
+4. style.css
+5. validator.js
+
+### DESCRIPCION DE ARCHIVOS
+
+1. *IMG, fue de las primer carpetas que realice ya que, ahi subi todas mis imagenes para poder realizar el html de mi tarjeta, lo primero que hice fue buscar cada una de las imagenes y diseños que me gustaban, que se parecieran a mi prototipo que habia realizado, las nombre para despues poder mandarlas a llamar. 
+
+2.*INDEX.HTML, fue en lo primero que empece a trabajar, para darle la estructura del contenido de mi proyecto, empece realizando el body, agrendando elementos en el como el contenedor y la imagenes que usaria para mi tarjeta, empece a usar clases y id, para despues porder mandarlos llamar de mi CSS Y JS, realice grupos flexbox para agrupar donde iba a ir el numero de tarjeta, nombre, fechas de caducidad, etc. tambien hice los formularios para la tarjeta y el boton para enviar la informaciòm.
+
+3.INDEX.JS, Me gusto mucho hacer esta pagina fue dificil, pero me gusto ver como funcionaba mi tarjeta y como funcionaba todo. 
+
+4.*STYLE.CSS, aqui en esta carpeta fue donde le di diseño a mi proyecto, creo que fue mi parte favorita porque me gusta la combinacion de colores y me gustaba analizar como acomodar todo para que viera armonico y quedara bonita la tarjeta, fue un poco complicado pero aprendi como crear fuentes  y como poner los colores. 
+
+5.*VALIDATOR.JS, fue la pagina que me costo, fue un reto aprender el algoritmo de Lunh ,me complique un poco, pero aprendi mucho. En este archivo se creo el algoritmo de lunh y el validador. El cual, exporte a mi archivo index.JS.
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,109 @@
 const validator = {
-  // ...
+// ...validacion de tarjeta //length = TAMAÑO
+  Isvalid: (cardNumber) => {
+    const IsEmpetyNumber = cardNumber.length === 0
+    if (IsEmpetyNumber) {
+      return false
+    }
+ 
+    //TRANSFORMAR EL NUEMERO DE TARJETA EN ARRAY [1,2,3,4,5,6,7,8,9,9,9,9,9,9,9,9]
+    const GetOriginalNumberArray = Array.from(cardNumber);
+
+    //INVERTIR LOS NUMEROS CON UNA CONSTANTE
+    const reverseNumber = GetOriginalNumberArray.reverse();
+
+    //CREAMOS CONSTANTE PARA AGREGAR UN ELEMENTO
+    const doubleNumber = []
+
+    //METODO forEach para obtener element 
+    reverseNumber.forEach((element,index) => {
+
+      //CONDICION PARA SABER SI LA POSICION ES IMPAR
+      if (index %2 !== 0 ) {
+
+        //CONSTANTE PARA MULTIPLICAR EL ELEMENT POR 2 SEGUN SU POSICION IMPAR
+        const elementImpar = element * 2;
+
+        //RETORNAR CON EL METODO PUSH PARA AGREGAR ELEMENTO AL ARRAY CREANDO doubleNumber
+        //METODO toString convierte un numero a un String
+        return doubleNumber.push(elementImpar.toString())
+      }
+
+      //AGREGAR ELEMENTOS A ARRAY QUE NO SON IMPAR
+      doubleNumber.push (element);
+    });
+
+    //CREAR UN ARRAY PARA SABER LA SUMA TOTAL
+    const arrayTotalAmount = [];
+
+    //RECORREMOS EL ARRAY PARA DOBLAR NUMERO
+    doubleNumber.forEach ((num) => {
+
+      //SE CREA VARIABLE PARA SUMAR DIGITO 
+      let addDigits = 0;
+
+      //CONDICIONAL SI EL NUMERO ES MAYOR A 9
+      //SI EL NUMERO ES 10 ENNTRA EN LA CONDICIONAL
+      if (num > 9) {
+      //CONSTANTE PARA SUMA DE LOS NUMERO Y CONVERTIMOS EL NUMERO EN UN ARRAY
+        const NewAadNumber =  Array.from (num); //ARRAY.FROM = METODO
+
+        //SE RECORRE ESTE ARRAY PARA SUMARLO ENTRE SI
+        NewAadNumber.forEach ((digit) => {
+          addDigits += Number(digit);
+        });
+      
+        //ARRAY CON EL METODO PUSH
+        return arrayTotalAmount.push(addDigits); 
+      }
+
+      //SI EL NUMERO ES MAYOR A 9 AGREGAMOS ARRAY PARA SUMAR EL TOTAL
+      arrayTotalAmount.push(Number(num));
+    });
+
+    //VARIABLE PARA SUMAR 
+    let totalAmount = 0;
+
+    //RECORREMOS EL ARRAY CON EL RESULTADO DE TODO EL PROCESO PARA SUMAR
+    arrayTotalAmount.forEach((element) => {
+
+      //SUMAMOS LOS ELEMENTOS PARA TENER EL VALOR TOTAL DE LAS TARJETAS
+      totalAmount += element;
+    });
+
+    //CONDICIONAL PARA SABER SI LA SUMA ES DIVISIBLE A 10 Y SABER SI ES VALIDA 
+    if (totalAmount % 10 === 0) return true;
+
+    //SI NO ES DIVISIBLE ES FALSO
+    else return false;
+  },
+
+  maskify: (cardNumber) => {
+    //VARIABLE QUE TRANSFORMA EL NUMERO DE TARJETA EN UN ARRAY
+    const cardNumberArray = Array.from(cardNumber);
+
+    //VARIABLE LET  PARA AGREGAR LOS CARACTERES #
+    let markedNumber = '';
+
+    //CICLO FOR PARA RECORRER DESDE 0 HASTA LA CANTIDAD DE NUMERO DE TARJETA -4
+    for (let i=0; i < cardNumberArray.length -4; i++) {
+        
+      //SE AGREGAN ### HASTA QUE TERMINE EL CICLO FOR 
+        
+      markedNumber += "#"
+    }
+
+    //METODO PARA SUBSTR ES PARA TOMAT ALGUN DETALLE DEL STRING POR EJEMPLO LOS ULTIMOS 4 
+    const lastFourNumber = cardNumber.substr (cardNumberArray.length - 4)
+
+    //AL NUMERO MARCADO LE AGREGAMOS LOS ULTIMOS 4 NUMEROS DE LA TARJETA  //+= (CONCATENAR)
+    markedNumber += lastFourNumber
+
+    //RETORNA EL NUMERO DE TARJETA EMASCARADO #### #### 7899
+    return markedNumber
+
+  },
+
 };
 
 export default validator;
